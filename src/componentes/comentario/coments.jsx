37221656d187b52eb9722fe274edab8bdd3b1c48
GeneralB5 <ianmarcobonfiglio@gmail.com>
2023-10-27 19:22:59 -0300

    Proyecto Final react js

--- src/componentes/comentario/coments.jsx
+++ src/componentes/comentario/coments.jsx
@@ -5,31 +5,31 @@ function comments(){
                     <div className="ContainerCommentario">
                         <div className="list-group-item list-group-item-action active py-3 lh-tight COMMENTS" aria-current="true">
                             <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong class="mb-1">List group item heading</strong>
+                                <strong class="mb-1">Juan</strong>
                                 <small>Wed</small>
                             </div>
-                            <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                            <div class="col-10 mb-1 small">No me disgusto el producto.</div>
                         </div>
                         <div className="list-group-item list-group-item-action active py-3 lh-tight COMMENTS" aria-current="true">
                             <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong className="mb-1">List group item heading</strong>
-                                <small>Wed</small>
+                                <strong className="mb-1">Maria</strong>
+                                <small>Tues</small>
                             </div>
-                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                            <div className="col-10 mb-1 small">Producto recomendadisimo.</div>
                         </div>
                         <div className="list-group-item list-group-item-action active py-3 lh-tight COMMENTS" aria-current="true">
                             <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong className="mb-1">List group item heading</strong>
-                                <small>Wed</small>
+                                <strong className="mb-1">Ramiro</strong>
+                                <small>Mon</small>
                             </div>
-                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                            <div className="col-10 mb-1 small">Me lo recomendo un amigo y me parecio muy buen producto, cuando lo probe aun mejor!!!</div>
                         </div>
                         <div className="list-group-item list-group-item-action active py-3 lh-tight COMMENTS" aria-current="true">
                             <div className="d-flex w-100 align-items-center justify-content-between">
-                                <strong className="mb-1">List group item heading</strong>
+                                <strong className="mb-1">__-matosx</strong>
                                 <small>Wed</small>
                             </div>
-                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                            <div className="col-10 mb-1 small">Not bad at all.</div>
                         </div>
                     </div>
                 </div>
